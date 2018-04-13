557b1ef82ce1b8a0c0f6e6266d8ebd6466c32f33
daw2 <daw2@daw2m08>
2018-04-13 19:40:21 +0200

    cookies3 commit 2

--- index.php
+++ index.php
@@ -4,20 +4,9 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html> 
-    <head> 
-        <meta charset="UTF­8"> 
-        <title>Desplegant una aplicació PHP sobre Heroku</title> 
-    </head> 
-    <body> 
-        <?php 
-            echo "Afegint Cookies<br>"; 
-            echo "Sóc el desenvolupador Raul Fernandez"; 
-            setcookie("nom", "Jordi Pons", time()+3600, "/", "", 0); 
-            setcookie("edat", "21", time()+3600, "/", "", 0); 
-            setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
-            setcookie("curs", "2n", time()+3600, "/", "", 0); 
-            setcookie("any", "2017­2018", time()+3600, "/", "", 0);            
-        ?>  
-    </body>    
-</html>
+<html> <head> <meta charset="UTF­8"> 
+        <title>Desplegant una aplicació PHP sobre Heroku</title> 
+    </head> 
+    <body> 
+        <?php echo "Afegint Cookies<br>"; 
+    echo "Sóc el desenvolupador Raul Fernandez"; setcookie("nom", "Jordi Pons", time()+3600, "/", "", 0); setcookie("edat", "21", time()+3600, "/", "", 0); setcookie("cicle", "DAW", time()+3600, "/", "", 0); setcookie("curs", "2n", time()+3600, "/", "", 0); setcookie("any", "2017­2018", time()+3600, "/", "", 0); ?> </body> </html>
